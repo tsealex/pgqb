1a2a8147064ed1ceec555b45cc90d9ec1b17fd60
Alexander Tse <tsealex@users.noreply.github.com>
2018-09-13 01:35:22 -0500

    Fix test

--- stmt_test.go
+++ stmt_test.go
@@ -11,13 +11,13 @@ func TestSelect(t *testing.T) {
 	c1 := Column(t1, "name")
 	c2 := Column(t1, "city")
 
-	sql := strings.Trim(NewContext().Select(c1, c2).ToSQL(), " ")
-	assert.Equal(t, `SELECT "school"."name", "school"."city" FROM "public"."school"`, sql)
+	sql := strings.Trim(NewContext().Select(c1, c2).Where(c2.Ne("New York City")).ToSQL(), " ")
+	assert.Equal(t, `SELECT "school"."name", "school"."city" FROM "public"."school" WHERE ("school"."city" != 'New York City')`, sql)
 
 	t2 := Table("public", "city")
 	e1 := Column(t2, "name")
 	e2 := Column(t2, "state")
-	sql = strings.Trim(NewContext().Select(c1, e1.As("city"), e2).ToSQL(), " ")
+	sql = strings.Trim(NewContext().Select(c1, e1.As("city"), e2).From(t1).ToSQL(), " ")
 	assert.Equal(t, `SELECT "school"."name", "city"."name" "city", "city"."state" FROM "public"."school", "public"."city"`, sql)
 
 
